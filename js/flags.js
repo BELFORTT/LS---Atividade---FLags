af3cda25dd07d4b85379047d50164f40e793cb63
Victor Belfort <victorbelfort10@hotmail.com>
2024-12-07 16:55:39 -0300

    first commit

---
+++ js/flags.js
@@ -0,0 +1,154 @@
+const flags = [
+    {
+      id: 'ad',
+      imagem: './imagens/ad.png',
+      nome: 'Andorra',
+    },
+    {
+      id: 'ae',
+      imagem: './imagens/ae.png',
+      nome: 'United Arab Emirates',
+    },
+    {
+      id: 'af',
+      imagem: './imagens/af.png',
+      nome: 'Afghanistan',
+    },
+    {
+      id: 'ag',
+      imagem: './imagens/ag.png',
+      nome: 'Antigua and Barbuda',
+    },
+    {
+      id: 'ai',
+      imagem: './imagens/ai.png',
+      nome: 'Anguilla',
+    },
+    {
+      id: 'al',
+      imagem: './imagens/al.png',
+      nome: 'Albania',
+    },
+    {
+      id: 'am',
+      imagem: './imagens/am.png',
+      nome: 'Armenia',
+    },
+    {
+      id: 'an',
+      imagem: './imagens/an.png',
+      nome: 'Netherlands Antilles',
+    },
+    {
+      id: 'ao',
+      imagem: './imagens/ao.png',
+      nome: 'Angola',
+    },
+    {
+      id: 'aq',
+      imagem: './imagens/aq.png',
+      nome: 'Antarctica',
+    },
+    {
+      id: 'ar',
+      imagem: './imagens/ar.png',
+      nome: 'Argentina',
+    },
+    {
+      id: 'as',
+      imagem: './imagens/as.png',
+      nome: 'American Samoa',
+    },
+    {
+      id: 'at',
+      imagem: './imagens/at.png',
+      nome: 'Austria',
+    },
+    {
+      id: 'au',
+      imagem: './imagens/au.png',
+      nome: 'Australia',
+    },
+    {
+      id: 'aw',
+      imagem: './imagens/aw.png',
+      nome: 'Aruba',
+    },
+    {
+      id: 'ax',
+      imagem: './imagens/ax.png',
+      nome: 'Åland Islands',
+    },
+    {
+      id: 'az',
+      imagem: './imagens/az.png',
+      nome: 'Azerbaijan',
+    },
+    {
+      id: 'ba',
+      imagem: './imagens/ba.png',
+      nome: 'Bosnia and Herzegovina',
+    },
+    {
+      id: 'bb',
+      imagem: './imagens/bb.png',
+      nome: 'Barbados',
+    },
+    {
+      id: 'bd',
+      imagem: './imagens/bd.png',
+      nome: 'Bangladesh',
+    },
+    {
+      id: 'be',
+      imagem: './imagens/be.png',
+      nome: 'Belgium',
+    },
+    {
+      id: 'bf',
+      imagem: './imagens/bf.png',
+      nome: 'Burkina Faso',
+    },
+    {
+      id: 'bg',
+      imagem: './imagens/bg.png',
+      nome: 'Bulgaria',
+    },
+    {
+      id: 'bh',
+      imagem: './imagens/bh.png',
+      nome: 'Bahrain',
+    },
+    {
+      id: 'bi',
+      imagem: './imagens/bi.png',
+      nome: 'Burundi',
+    },
+    {
+      id: 'bj',
+      imagem: './imagens/bj.png',
+      nome: 'Benin',
+    },
+    {
+      id: 'bl',
+      imagem: './imagens/bl.png',
+      nome: 'Saint Barthélemy',
+    },
+    {
+      id: 'bm',
+      imagem: './imagens/bm.png',
+      nome: 'Bermuda',
+    },
+    {
+      id: 'bn',
+      imagem: './imagens/bn.png',
+      nome: 'Brunei Darussalam',
+    },
+    {
+      id: 'bo',
+      imagem: './imagens/bo.png',
+      nome: 'Bolivia',
+    },
+]
+
+export default flags;
---
+++ js/main.js
@@ -0,0 +1,12 @@
+import Flags from "./flags.js";
+
+const main = document.getElementById("local_flags");
+
+main.innerHTML = Flags.map(flag => `
+  <div>
+    <img src="${flag.imagem}" alt="${flag.nome}">
+    <p>${flag.nome}</p>
+  </div>
+  `).join("");
+
+
